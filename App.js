30250bafacbfc37855aa472bf0c2ca4ed5d924da
Dominic Hains <contact.dominichains@gmail.com>
2021-07-11 17:28:14 -0400

    feat: Landing page with options

--- App.js
+++ App.js
@@ -1,21 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
+  const [deviceId, setDeviceId] = useState('');
+
+  useEffect(() => {
+    // Check if the device has a uuid in memory
+    // If it hasn't, will create one and send it to firebase to create a new user.
+    const id = uuidv4();
+    setDeviceId(id);
+  }, [setDeviceId]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => console.log('new path')}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}> Start a new path </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => console.log('guardians')}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}> My Guardians </Text>
+      </TouchableOpacity>
+      <SafeAreaView style={styles.guardianIdContainer}>
+        <Text style={styles.guardianIdText}>Your guardian ID </Text>
+        {deviceId && <QRCode height={500} width={500} value={deviceId} />}
+      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8EAED',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    width: 290,
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  buttonText: {
+    fontSize: 14,
+  },
+  guardianIdContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    width: 200,
+    height: 200,
+    alignItems: 'center',
+    marginTop: 35,
+  },
+  guardianIdText: {
+    marginVertical: 15,
+    fontSize: 16,
   },
 });
---
+++ assets/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const Email = styled.TextInput`
+  padding-vertical: 15px;
+  padding-horizontal: 15px;
+  background-color: #e8eaed;
+  border-radius: 5px;
+  width: 290px;
+`;
+
+export const Password = styled.TextInput`
+  padding-vertical: 15px;
+  padding-horizontal: 15px;
+  background-color: #e8eaed;
+  border-radius: 5px;
+  width: 290px;
+  margin-top: 15px;
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  padding-vertical: 15px;
+  padding-horizontal: 15px;
+  background-color: #ee504c;
+  border-radius: 5px;
+  border-color: #c0c0c0;
+  width: 200px;
+  margin-top: 25px;
+`;
